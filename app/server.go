badac03e8b471ae8c5ad688610308362d5f595fb
Vasu Bansal <vasubansal1998@gmail.com>
2024-04-08 18:21:12 +0530

    [Vasu] added file path handling to send file content as response

--- app/server.go
+++ app/server.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net"
 	"os"
+	"path"
 	"strings"
 )
 
@@ -14,8 +15,14 @@ const (
 
 	ECHO_PATH       = "/echo/"
 	USER_AGENT_PATH = "/user-agent"
-	USER_AGENT      = "User-Agent"
+	FILE_PATH       = "/files/"
+
+	USER_AGENT   = "User-Agent"
+	PLAIN_TEXT   = "text/plain"
+	OCTET_STREAM = "application/octet-stream"
 )
+
+var WORKING_DIRECTORY string
 
 func readRequestString(conn net.Conn) string {
 	readBuffer := make([]byte, 1024)
@@ -34,12 +41,26 @@ func respondToHttpRequest(conn net.Conn, r *httpRequest) {
 	} else if strings.HasPrefix(r.Path, ECHO_PATH) {
 		response.StatusCode = 200
 		responseBody := r.Path[len(ECHO_PATH):]
-		response.addBody("text/plain", responseBody)
+		response.addBody(PLAIN_TEXT, responseBody)
 	} else if strings.HasPrefix(r.Path, USER_AGENT_PATH) {
 		response.StatusCode = 200
 		responseBody := r.Headers[USER_AGENT]
 
-		response.addBody("text/plain", responseBody)
+		response.addBody(PLAIN_TEXT, responseBody)
+	} else if strings.HasPrefix(r.Path, FILE_PATH) {
+		filePath := path.Join(WORKING_DIRECTORY, r.Path[len(FILE_PATH):])
+		if _, err := os.Stat(filePath); err != nil {
+			response.StatusCode = 404
+		} else {
+			response.StatusCode = 200
+			fileContent, err := os.ReadFile(filePath)
+			if err != nil {
+				logAndThrowError(err, fmt.Sprintf("Error while reading file: %s", filePath))
+			}
+
+			responseBody := string(fileContent)
+			response.addBody(OCTET_STREAM, responseBody)
+		}
 	} else {
 		// not found
 		response.StatusCode = 404
@@ -66,6 +87,12 @@ func handleConnection(conn net.Conn) {
 }
 
 func main() {
+	if len(os.Args) < 2 {
+		WORKING_DIRECTORY, _ = os.Getwd()
+	} else {
+		WORKING_DIRECTORY = os.Args[2]
+	}
+
 	l, err := net.Listen("tcp", fmt.Sprintf("%s:%d", TCP_HOST, TCP_PORT))
 	if err != nil {
 		logAndThrowError(err, fmt.Sprintf("Failed to bind to port: %d", TCP_PORT))
